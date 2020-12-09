c5f4442ca8dd9c150feab5045d8feea0f75f8afe
Ostap <o.tomash650416@gmail.com>
2020-12-09 20:56:25 +0200

    add some variables

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
 const generateBlocks = () => {
+    const littleSquaresInLine = 5;
+    const countOfLittleSquare = 25;
+    const littleSquareWidth = 50;
+    const littleSquareHeight = 50;
     const bigSquare = document.createElement('div');
     bigSquare.classList.add('bigSquare');
-    bigSquare.style.width = 5 * 50 + 'px';
+    bigSquare.style.width = littleSquaresInLine * littleSquareWidth + 'px';
     bigSquare.style.display = 'flex';
     bigSquare.style.margin = "100px auto";
     bigSquare.style.flexWrap = 'wrap';
     document.body.prepend(bigSquare);
 
-    const countOfLittleSquare = 25;
-
     for(let i = 1; i <= countOfLittleSquare; i++) {
         const littleSquare = document.createElement('div');
         littleSquare.classList.add('littleSquare');
-        littleSquare.style.width = 50 + 'px';
-        littleSquare.style.height = 50 + 'px';
+        littleSquare.style.width = littleSquareWidth + 'px';
+        littleSquare.style.height = littleSquareHeight + 'px';
         littleSquare.style.backgroundColor = '#' + (Math.random().toString(16) + '000000').substring(2,8).toUpperCase();
         bigSquare.append(littleSquare);
     }
